41595340c797d920cdb651c4a3df63030e4b9583
sainiteshGit <nitesh_4u@yahoo.com>
2022-09-20 18:24:04 -0400

    stop words reading from file

---
+++ Search-Engine-master/src/main/java/informationRetrieval/StopWords.java
@@ -0,0 +1,32 @@
+import java.util.Set;
+import java.util.HashSet;
+import java.io.*;
+class StopWords {
+    public static void main(String args[]){
+
+        String path="/Users/SaiNitesh/Hackathon/stopWords.txt";
+
+        Set<String> stopWords= getStoprWords(path);
+        System.out.println("stop words are::");
+        for(String word:stopWords){
+             System.out.println(word);
+        }
+
+    }
+
+
+    public static Set<String> getStoprWords(String path){
+        Set<String> words = new HashSet<String>();
+        try(BufferedReader reader = new BufferedReader(new FileReader(path))){
+         for(String line = reader.readLine(); line != null; line = reader.readLine()) {
+             line=line.replaceAll("\\s", "");
+             words.add(line);
+         }
+        }
+        catch(Exception e){
+             System.out.println("error while reading file");
+        }
+        return words;
+    }
+
+}
